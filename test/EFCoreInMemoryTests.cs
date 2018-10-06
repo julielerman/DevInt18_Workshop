5c9334a4fd2827613c20f132f838a017ab32fb32
Julie Lerman <julielerman@gmail.com>
2018-10-06 09:33:57 -0400

    revised m2m test

--- test/EFCoreInMemoryTests.cs
+++ test/EFCoreInMemoryTests.cs
@@ -8,7 +8,7 @@ using Xunit;
 
 namespace test {
     public class EFCoreInMemoryTests {
-     
+
         private static Team CreateTeamAjax () {
             return new Team ("AFC Ajax", "The Lancers", "1900", "Amsterdam Arena");
         }
@@ -26,100 +26,104 @@ namespace test {
                 Assert.Equal ("AFC Ajax", storedTeam.TeamName);
             }
         }
+
         [Fact]
-        public void CanStoreAndRetrievePlayerName()
-        {
-              var team = CreateTeamAjax ();
+        public void CanStoreAndRetrievePlayerName () {
+            var team = CreateTeamAjax ();
             team.AddPlayer ("André", "Onana", out string response);
-          
+
             var options = new DbContextOptionsBuilder<TeamContext> ().UseInMemoryDatabase ("playername").Options;
             using (var context = new TeamContext (options)) {
                 context.Teams.Add (team);
                 context.SaveChanges ();
             }
-            using (var context=new TeamContext(options))
-            {
+            using (var context = new TeamContext (options)) {
                 //note a current bug:
                 //https://github.com/aspnet/EntityFrameworkCore/issues/9210
                 //requires a workaround of including the owned entity of an included navigation property
-                var storedTeam=context.Teams.Include(t=>t.Players).ThenInclude(p=>p.NameFactory).FirstOrDefault();
-                Assert.Single(storedTeam.Players);
-                Assert.Equal("André Onana", storedTeam.Players.First().Name );
-          
+                var storedTeam = context.Teams.Include (t => t.Players).ThenInclude (p => p.NameFactory).FirstOrDefault ();
+                Assert.Single (storedTeam.Players);
+                Assert.Equal ("André Onana", storedTeam.Players.First ().Name);
+
             }
         }
-       
+
         [Fact]
         public void CanStoreAndRetrieveTeamPlayers () {
             var team = CreateTeamAjax ();
             team.AddPlayer ("André", "Onana", out string response);
-          
+
             var options = new DbContextOptionsBuilder<TeamContext> ().UseInMemoryDatabase ("storeretrieveplayer").Options;
             using (var context = new TeamContext (options)) {
                 context.Teams.Add (team);
                 context.SaveChanges ();
             }
-             using (var context = new TeamContext (options)) {
-                var storedTeam=context.Teams.Include(t=>t.Players).FirstOrDefault();
-                Assert.Single(storedTeam.Players);
-              }
+            using (var context = new TeamContext (options)) {
+                var storedTeam = context.Teams.Include (t => t.Players).FirstOrDefault ();
+                Assert.Single (storedTeam.Players);
+            }
         }
-         [Fact]
+
+        [Fact]
         public void TeamPreventsAddingPlayersToExistingTeamWhenPlayersNotInMemory () {
             var team = CreateTeamAjax ();
             team.AddPlayer ("André", "Onana", out string response);
-          
+
             var options = new DbContextOptionsBuilder<TeamContext> ().UseInMemoryDatabase ("preventplayeronteamwithplayersnotloaded").Options;
             using (var context = new TeamContext (options)) {
                 context.Teams.Add (team);
                 context.SaveChanges ();
             }
-             using (var context = new TeamContext (options)) {
-                var storedTeam=context.Teams.FirstOrDefault();
+            using (var context = new TeamContext (options)) {
+                var storedTeam = context.Teams.FirstOrDefault ();
                 storedTeam.AddPlayer ("Matthijs", "de Ligt", out response);
-                Assert.Equal("You must first retrieve",response.Substring(0,23));
-             }
+                Assert.Equal ("You must first retrieve", response.Substring (0, 23));
+            }
         }
+
         [Fact]
-         public void TeamAllowsAddingPlayersToExistingTeamWhenPlayersAreLoaded () {
+        public void TeamAllowsAddingPlayersToExistingTeamWhenPlayersAreLoaded () {
             var team = CreateTeamAjax ();
             team.AddPlayer ("André", "Onana", out string response);
-          
+
             var options = new DbContextOptionsBuilder<TeamContext> ().UseInMemoryDatabase ("allowplayeronteamwithplayersloaded").Options;
             using (var context = new TeamContext (options)) {
                 context.Teams.Add (team);
                 context.SaveChanges ();
             }
-             using (var context = new TeamContext (options)) {
-              var storedTeam=context.Teams.Include(t=>t.Players).ThenInclude(p=>p.NameFactory).FirstOrDefault();
-              storedTeam.AddPlayer ("Matthijs", "de Ligt", out response);
-                Assert.Equal(2,storedTeam.Players.Count());
-             }
+            using (var context = new TeamContext (options)) {
+                var storedTeam = context.Teams.Include (t => t.Players).ThenInclude (p => p.NameFactory).FirstOrDefault ();
+                storedTeam.AddPlayer ("Matthijs", "de Ligt", out response);
+                Assert.Equal (2, storedTeam.Players.Count ());
+            }
         }
 
-           [Fact]
-         public void CanStoreAndRetrieveManagerTeamHistory()
-         {
+        [Fact]
+        public void CanStoreAndRetrieveManagerTeamHistory () {
+            var options = new DbContextOptionsBuilder<TeamContext> ().UseInMemoryDatabase ("CanStoreAndRetrieveManagerTeamHistory").Options;
             var team = CreateTeamAjax ();
             team.AddPlayer ("André", "Onana", out string response);
-            var firstmanager=new Manager ("Marcel", "Keizer");
+            var firstmanager = new Manager ("Marcel", "Keizer");
             team.ChangeManagement (firstmanager);
-             team.ChangeManagement (new Manager ("Erik", "ten Hag"));
-              
-            var options = new DbContextOptionsBuilder<TeamContext> ().UseInMemoryDatabase ("storemanagerhistory").Options;
+            var secondmanager = new Manager ("Erik", "ten Hag");
+            team.ChangeManagement (secondmanager);
+            team.ChangeManagement (new Manager ("Christian", "Weyer"));
+
             using (var context = new TeamContext (options)) {
-                context.AddRange (team,firstmanager);
+                context.Database.EnsureDeleted ();
+                context.Database.EnsureCreated ();
+                context.AddRange (team, firstmanager, secondmanager);
                 context.SaveChanges ();
             }
-             using (var context = new TeamContext (options)) {
-                     var M1=context.Managers.Include(m=>m.PastTeams).FirstOrDefault(m=>m.NameFactory.Last=="Keizer");
-                 var M2=context.Managers.Include(m=>m.PastTeams).FirstOrDefault(m=>m.NameFactory.Last=="ten Hag");
-                 Assert.Equal(new{M1="Marcel Keizer",M1Count=1,M2="Erik ten Hag",M2Count=0},
-                  new{M1=M1.Name, M1Count=M1.PastTeams.Count, M2=M2.Name, M2Count=M2.PastTeams.Count} );
+            using (var context = new TeamContext (options)) {
+                var managers = context.Managers.Include (m => m.PastTeams).ToList ();
+                Assert.Equal (3, managers.Count);
+                foreach (var manager in managers) {
+                    //will display when debugging
+                    Console.WriteLine ($"{manager.Name}: {manager.PastTeams.Count} Past Teams");
+                }
             }
 
-         }
-        
-
+        }
     }
 }
