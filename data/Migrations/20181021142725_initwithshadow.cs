8dc52d56b4f7307783a70ecd68fe8cd31296f1a0
julie <julielerman@gmail.com>
2018-10-21 16:37:20 +0200

    trash old migrations, create new ones

---
+++ data/Migrations/20181021142725_initwithshadow.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace data.Migrations
+{
+    public partial class initwithshadow : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "UniformColors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    ShirtPrimary = table.Column<string>(nullable: false),
+                    ShirtSecondary = table.Column<string>(nullable: false),
+                    ShirtTertiary = table.Column<string>(nullable: false),
+                    ShortsPrimary = table.Column<string>(nullable: false),
+                    ShortsSecondary = table.Column<string>(nullable: false),
+                    Socks = table.Column<string>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UniformColors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teams",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    TeamName = table.Column<string>(nullable: true),
+                    Nickname = table.Column<string>(nullable: true),
+                    YearFounded = table.Column<string>(nullable: true),
+                    HomeStadium = table.Column<string>(nullable: true),
+                    HomeColorsId = table.Column<int>(nullable: true),
+                    Created = table.Column<DateTime>(nullable: false),
+                    LastModified = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teams", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Teams_UniformColors_HomeColorsId",
+                        column: x => x.HomeColorsId,
+                        principalTable: "UniformColors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Managers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    NameFactory_First = table.Column<string>(nullable: true),
+                    NameFactory_Last = table.Column<string>(nullable: true),
+                    CurrentTeamId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Managers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Managers_Teams_CurrentTeamId",
+                        column: x => x.CurrentTeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Player",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    NameFactory_First = table.Column<string>(nullable: true),
+                    NameFactory_Last = table.Column<string>(nullable: true),
+                    TeamId = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Player", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Player_Teams_TeamId",
+                        column: x => x.TeamId,
+                        principalTable: "Teams",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ManagerTeamHistory",
+                columns: table => new
+                {
+                    ManagerId = table.Column<Guid>(nullable: false),
+                    TeamId = table.Column<Guid>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ManagerTeamHistory", x => new { x.ManagerId, x.TeamId });
+                    table.ForeignKey(
+                        name: "FK_ManagerTeamHistory_Managers_ManagerId",
+                        column: x => x.ManagerId,
+                        principalTable: "Managers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Managers_CurrentTeamId",
+                table: "Managers",
+                column: "CurrentTeamId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Player_TeamId",
+                table: "Player",
+                column: "TeamId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teams_HomeColorsId",
+                table: "Teams",
+                column: "HomeColorsId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ManagerTeamHistory");
+
+            migrationBuilder.DropTable(
+                name: "Player");
+
+            migrationBuilder.DropTable(
+                name: "Managers");
+
+            migrationBuilder.DropTable(
+                name: "Teams");
+
+            migrationBuilder.DropTable(
+                name: "UniformColors");
+        }
+    }
+}
