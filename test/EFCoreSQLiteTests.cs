32e17c59363088ead3f633a1525e104a8edf9849
julie <julielerman@gmail.com>
2018-10-21 22:48:56 +0200

    clean note

--- test/EFCoreSQLiteTests.cs
+++ test/EFCoreSQLiteTests.cs
@@ -6,38 +6,29 @@ using Domain;
 using Microsoft.EntityFrameworkCore;
 using Xunit;
 
-namespace test
-{
-    public class EFCoreSQLiteTests
-    {
+namespace test {
+    public class EFCoreSQLiteTests {
 
-        //no need for SQLite ref in this project. The data project uses it by default.
-        private static Team CreateTeamAjax()
-        {
-            return new Team("AFC Ajax", "The Lancers", "1900", "Amsterdam Arena");
+        private static Team CreateTeamAjax () {
+            return new Team ("AFC Ajax", "The Lancers", "1900", "Amsterdam Arena");
         }
 
-       
-
         [Fact]
-        public void CanStoreAndRetrieveHomeColors()
-        {
-            var team = CreateTeamAjax();
-            team.SpecifyHomeUniformColors(Color.Blue, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White);
+        public void CanStoreAndRetrieveHomeColors () {
+            var team = CreateTeamAjax ();
+            team.SpecifyHomeUniformColors (Color.Blue, Color.Red, Color.Empty, Color.White, Color.Empty, Color.White);
 
-            using (var context = new TeamContext())
-            {
-                context.Database.EnsureDeleted();
-                context.Database.EnsureCreated();
-                context.Teams.Add(team);
-                context.SaveChanges();
+            using (var context = new TeamContext ()) {
+                context.Database.EnsureDeleted ();
+                context.Database.EnsureCreated ();
+                context.Teams.Add (team);
+                context.SaveChanges ();
             }
-        
-            using (var context = new TeamContext())
-            {
-                var storedTeam = context.Teams.Include(t => t.HomeColors).FirstOrDefault();
 
-                Assert.Equal(Color.Blue, storedTeam.HomeColors.ShirtPrimary);
+            using (var context = new TeamContext ()) {
+                var storedTeam = context.Teams.Include (t => t.HomeColors).FirstOrDefault ();
+
+                Assert.Equal (Color.Blue, storedTeam.HomeColors.ShirtPrimary);
             }
         }
 
